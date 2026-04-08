97b9d4664743dfbd7b8d3f070a388b05d552c5cf
accen-g <gaya.accen@gmail.com>
2026-04-08 21:35:34 +0530

    feature: Model Changes

--- bic-ccd/backend/app/database.py
+++ bic-ccd/backend/app/database.py
@@ -1,5 +1,5 @@
 """Database engine, session factory and dependency."""
-from sqlalchemy import create_engine, event
+from sqlalchemy import create_engine, event, MetaData
 from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 from app.config import get_settings
 
@@ -27,8 +27,17 @@ if settings.USE_SQLITE:
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
+_naming_convention = {
+    "ix": "ix_%(table_name)s_%(column_0_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=_naming_convention)
 
 
 def get_db():
